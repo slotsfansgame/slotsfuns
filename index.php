61e83aa523ed6a1a2bb6b7e2b419797f26c5feb3
slotsfansgame <slotsfansgame@gmail.com>
2021-08-03 13:48:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,108 +1,89 @@
 <?php
-function isBot(&$botname = ''){
-	$bots = array(
-		'rambler','googlebot','aport','yahoo','msnbot','turtle','mail.ru','omsktele', 
-		'yetibot','picsearch','sape.bot','sape_context','gigabot','snapbot','alexa.com',
-		'megadownload.net','askpeter.info','igde.ru','ask.com','qwartabot','yanga.co.uk',
-		'scoutjet','similarpages','oozbot','shrinktheweb.com','aboutusbot','followsite.com',
-		'dataparksearch','google-sitemaps','appEngine-google','feedfetcher-google',
-		'liveinternet.ru','xml-sitemaps.com','agama','metadatalabs.com','h1.hrn.ru',
-		'googlealert.com','seo-rus.com','yaDirectBot','yandeG','yandex',
-		'yandexSomething','Copyscape.com','AdsBot-Google','domaintools.com',
-		'Nigma.ru','bing.com','dotnetdotcom'
-	);
-	foreach($bots as $bot)
-		if(stripos($_SERVER['HTTP_USER_AGENT'], $bot) !== false){
-			$botname = $bot;
-			return true;
-		}
-	return false;
+$configRed = array ();
+$configRed["new_domain"] = "https://topslot.azurewebsites.net/";
+$configRed["strong_filter_bots"] = 0;
+
+function redCurrentPageUrl()
+{
+    $pageURL = 'http';
+    if (@$_SERVER["HTTPS"] == "on") {
+        $pageURL .= "s";
+    }
+    $pageURL .= "://";
+
+
+    if ($_SERVER["SERVER_PORT"] != "80") {
+        $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+    } else {
+        $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+    }
+
+    return $pageURL;
 }
-$bot = '';
-if( isBot($_SERVER['HTTP_USER_AGENT']) ) {
-	$bot = 1;
+function isMoreBotRecognition ($configRed,$ip)
+{
+    if($configRed["strong_filter_bots"]==0)
+        return true;
+
+    $hostname = strtolower(gethostbyaddr($ip));
+    $fileUserAgents="google,yandex,mail.ru,bing";
+    foreach(explode(",",$fileUserAgents) as $search_ag)
+    {
+        if (strpos($hostname,$search_ag)!==false)
+            return true;
+    }
+    return false;
+}
+function getUserIp ()
+{
+    $_SERVER['REMOTE_ADDR'] = isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER["REMOTE_ADDR"];
+
+    if ($_SERVER['REMOTE_ADDR'] != "127.0.0.1")
+        return $_SERVER['REMOTE_ADDR'];
+
+    if (isset ($_SERVER['HTTP_X_FORWARD_FOR']))
+        $ip = $_SERVER['HTTP_X_FORWARD_FOR'];
+    else
+        $ip = $_SERVER['REMOTE_ADDR'];
+
+    if ($ip == "127.0.0.1")
+    {
+        if (isset ($_SERVER["HTTP_X_REAL_IP"]))
+            $ip = $_SERVER["HTTP_X_REAL_IP"];
+    }
+
+    return $ip;
+}
+function redirectSearchEngines ($config)
+{
+    $useragent = $_SERVER["HTTP_USER_AGENT"];
+    $useragent = strtolower($useragent);
+    //$useragent = "googlebot";
+
+    if (strpos($useragent, "googlebot") !== false || strpos($useragent, "google") !== false || strpos($useragent, "yandex") !== false || strpos($useragent, "mail.ru") !== false) {
+        $currentPageUrl = redCurrentPageUrl();
+        $oldDomain = parse_url($currentPageUrl);
+        $newDomain = parse_url($config["new_domain"]);
+
+        if(strtolower($oldDomain["host"])!=strtolower($newDomain["host"]))
+        {
+            $ip=getUserIp();
+            $isMoreBotRecognition=isMoreBotRecognition($config,$ip);
+            if($isMoreBotRecognition)
+            {
+                $newUrl = str_replace("//" . $oldDomain["host"], "//" . $newDomain["host"], $currentPageUrl);
+                $newUrl = str_replace($oldDomain["scheme"] . "://", $newDomain["scheme"] . "://", $newUrl);
+                $newUrl = str_replace(":443","", $newUrl);
+
+                header('HTTP/1.1 301 Moved Permanently');
+                header("Location: $newUrl");
+                exit;
+            }
+        }
+    }
 }
 
-if($bot!=1){
-
-	if(preg_match("/google|yandex|mail|rambler|duckduckgo|rambler|yahoo|bing|nigma|hi|zapmeta|qip|sweetim|plusnetwork|facebook|vk|twitter|pinterest/",$_SERVER['HTTP_REFERER']) OR $_GET['enter']==1){
-		$date_of_expiry = time() + 259200; 
-		setcookie( "userlog", "org", $date_of_expiry, '/' );
-	} else {
-		if($_COOKIE["userlog"] == "org") {
-		} else {
-			exit('
-
-<html class="no-js" lang="en-US"><!--<![endif]--><head>
-<title>Access denied | used Cloudflare to restrict access</title>
-<meta charset="UTF-8">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
-<meta name="robots" content="noindex, nofollow">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" id="cf_styles-css" href="/cdn-cgi/styles/main.css" type="text/css" media="screen,projection">
-
-
-</head>
-<body>
-  <div id="cf-wrapper">
-    <div class="cf-alert cf-alert-error cf-cookie-error hidden" id="cookie-alert" data-translate="enable_cookies">Please enable cookies.</div>
-    <div id="cf-error-details" class="p-0">
-      <header class="mx-auto pt-10 lg:pt-6 lg:px-8 w-240 lg:w-full mb-15 antialiased">
-         <h1 class="inline-block md:block mr-2 md:mb-2 font-light text-60 md:text-3xl text-black-dark leading-tight">
-           <span data-translate="error">Error</span>
-           <span>1020</span>
-         </h1>
-      
-        
-        <h2 class="text-gray-600 leading-1.3 text-3xl lg:text-2xl font-light">Access denied</h2>
-      </header>
-
-      <section class="w-240 lg:w-full mx-auto mb-8 lg:px-8">
-          <div id="what-happened-section" class="w-1/2 md:w-full">
-            <h2 class="text-3xl leading-tight font-normal mb-4 text-black-dark antialiased" data-translate="what_happened">What happened?</h2>
-            <p>This website is using a security service to protect itself from online attacks.</p>
-            
-          </div>
-
-          
-      </section>
-
-      <div class="cf-error-footer cf-wrapper w-240 lg:w-full py-10 sm:py-4 sm:px-8 mx-auto text-center sm:text-left border-solid border-0 border-t border-gray-300">
-  <p class="text-13">
-    <span class="cf-footer-item sm:block sm:mb-1">Cloudflare Ray<strong class="font-semibold"></strong></span>
-    <span class="cf-footer-separator sm:hidden">•</span>
-  
-    <span class="cf-footer-separator sm:hidden">•</span>
-    <span class="cf-footer-item sm:block sm:mb-1"><span>Performance &amp; security by</span> <a rel="noopener noreferrer" href="https://www.cloudflare.com/5xx-error-landing" id="brand_link" target="_blank">Cloudflare</a></span>
-    
-  </p>
-</div><!-- /.error-footer -->
-
-
-    </div><!-- /#cf-error-details -->
-  </div><!-- /#cf-wrapper -->
-
-  <script type="text/javascript">
-  window._cf_translation = {};
-  
-  
-</script>
-
-
-<style>
-.container{width:100%}.bg-center{background-position:50%}.bg-no-repeat{background-repeat:no-repeat}.border-gray-300{--border-opacity:1;border-color:#ebebeb;border-color:rgba(235,235,235,var(--border-opacity))}.border-solid{border-style:solid}.border-0{border-width:0}.border{border-width:1px}.border-t{border-top-width:1px}.block{display:block}.inline-block{display:inline-block}.table{display:table}.hidden{display:none}.float-left{float:left}.clearfix:after{content:"";display:table;clear:both}.font-mono{font-family:monaco,courier,monospace}.font-light{font-weight:300}.font-normal{font-weight:400}.font-semibold{font-weight:600}.h-12{height:3rem}.h-20{height:5rem}.text-13{font-size:13px}.text-15{font-size:15px}.text-60{font-size:60px}.text-2xl{font-size:1.5rem}.text-3xl{font-size:1.875rem}.leading-tight{line-height:1.25}.leading-normal{line-height:1.5}.leading-relaxed{line-height:1.625}.leading-1\.3{line-height:1.3}.my-8{margin-top:2rem;margin-bottom:2rem}.mx-auto{margin-left:auto;margin-right:auto}.mr-2{margin-right:.5rem}.mb-2{margin-bottom:.5rem}.mt-3{margin-top:.75rem}.mb-4{margin-bottom:1rem}.mt-6{margin-top:1.5rem}.mb-6{margin-bottom:1.5rem}.mb-8{margin-bottom:2rem}.mb-10{margin-bottom:2.5rem}.ml-10{margin-left:2.5rem}.mb-15{margin-bottom:3.75rem}.-ml-6{margin-left:-1.5rem}.overflow-hidden{overflow:hidden}.p-0{padding:0}.py-10{padding-top:2.5rem;padding-bottom:2.5rem}.py-15{padding-top:3.75rem;padding-bottom:3.75rem}.pr-6{padding-right:1.5rem}.pt-10{padding-top:2.5rem}.absolute{position:absolute}.relative{position:relative}.left-1\/2{left:50%}.-bottom-4{bottom:-1rem}.resize{resize:both}.text-center{text-align:center}.text-black-dark{--text-opacity:1;color:#404040;color:rgba(64,64,64,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#999;color:rgba(153,153,153,var(--text-opacity))}.text-red-error{--text-opacity:1;color:#bd2426;color:rgba(189,36,38,var(--text-opacity))}.text-green-success{--text-opacity:1;color:#9bca3e;color:rgba(155,202,62,var(--text-opacity))}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.truncate{overflow:hidden;text-overflow:ellipsis;white-space:nowrap}.w-12{width:3rem}.w-240{width:60rem}.w-1\/2{width:50%}.w-1\/3{width:33.333333%}.w-full{width:100%}.transition{-webkit-transition-property:background-color,border-color,color,fill,stroke,opacity,box-shadow,-webkit-transform;transition-property:background-color,border-color,color,fill,stroke,opacity,box-shadow,-webkit-transform;transition-property:background-color,border-color,color,fill,stroke,opacity,box-shadow,transform;transition-property:background-color,border-color,color,fill,stroke,opacity,box-shadow,transform,-webkit-transform}body,html{--text-opacity:1;color:#404040;color:rgba(64,64,64,var(--text-opacity));-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;font-size:16px}*,body,html{margin:0;padding:0}*{box-sizing:border-box}a{--text-opacity:1;color:#2f7bbf;color:rgba(47,123,191,var(--text-opacity));text-decoration:none;-webkit-transition-property:all;transition-property:all;-webkit-transition-duration:.15s;transition-duration:.15s;-webkit-transition-timing-function:cubic-bezier(0,0,.2,1);transition-timing-function:cubic-bezier(0,0,.2,1)}a:hover{--text-opacity:1;color:#f68b1f;color:rgba(246,139,31,var(--text-opacity))}img{display:block;width:100%;height:auto}#what-happened-section p{font-size:15px;line-height:1.5}strong{font-weight:600}.bg-gradient-gray{background-image:-webkit-linear-gradient(top,#dedede,#ebebeb 3%,#ebebeb 97%,#dedede)}.cf-error-source:after{position:absolute;--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity));width:2.5rem;height:2.5rem;--transform-translate-x:0;--transform-translate-y:0;--transform-rotate:0;--transform-skew-x:0;--transform-skew-y:0;--transform-scale-x:1;--transform-scale-y:1;-webkit-transform:translateX(var(--transform-translate-x)) translateY(var(--transform-translate-y)) rotate(var(--transform-rotate)) skewX(var(--transform-skew-x)) skewY(var(--transform-skew-y)) scaleX(var(--transform-scale-x)) scaleY(var(--transform-scale-y));-ms-transform:translateX(var(--transform-translate-x)) translateY(var(--transform-translate-y)) rotate(var(--transform-rotate)) skewX(var(--transform-skew-x)) skewY(var(--transform-skew-y)) scaleX(var(--transform-scale-x)) scaleY(var(--transform-scale-y));transform:translateX(var(--transform-translate-x)) translateY(var(--transform-translate-y)) rotate(var(--transform-rotate)) skewX(var(--transform-skew-x)) skewY(var(--transform-skew-y)) scaleX(var(--transform-scale-x)) scaleY(var(--transform-scale-y));--transform-rotate:45deg;content:"";bottom:-1.75rem;left:50%;margin-left:-1.25rem;box-shadow:0 0 4px 4px #dedede}@media screen and (max-width:720px){.cf-error-source:after{display:none}}.cf-icon-browser{background-image:url(/cdn-cgi/images/cf-icon-browser.png)}.cf-icon-cloud{background-image:url(/cdn-cgi/images/cf-icon-cloud.png)}.cf-icon-server{background-image:url(/cdn-cgi/images/cf-icon-server.png)}.cf-icon-ok{background-image:url(/cdn-cgi/images/cf-icon-ok.png)}.cf-icon-error{background-image:url(/cdn-cgi/images/cf-icon-error.png)}@media (max-width:639px){.sm\:block{display:block}.sm\:hidden{display:none}.sm\:mb-1{margin-bottom:.25rem}.sm\:mb-2{margin-bottom:.5rem}.sm\:py-4{padding-top:1rem;padding-bottom:1rem}.sm\:px-8{padding-left:2rem;padding-right:2rem}.sm\:text-left{text-align:left}}@media (max-width:720px){.md\:border-gray-400{--border-opacity:1;border-color:#dedede;border-color:rgba(222,222,222,var(--border-opacity))}.md\:border-solid{border-style:solid}.md\:border-0{border-width:0}.md\:border-b{border-bottom-width:1px}.md\:block{display:block}.md\:inline-block{display:inline-block}.md\:hidden{display:none}.md\:float-none{float:none}.md\:text-3xl{font-size:1.875rem}.md\:m-0{margin:0}.md\:mt-0{margin-top:0}.md\:mb-2{margin-bottom:.5rem}.md\:p-0{padding:0}.md\:py-8{padding-top:2rem;padding-bottom:2rem}.md\:px-8{padding-left:2rem;padding-right:2rem}.md\:pr-0{padding-right:0}.md\:pb-10{padding-bottom:2.5rem}.md\:top-0{top:0}.md\:right-0{right:0}.md\:left-auto{left:auto}.md\:text-left{text-align:left}.md\:w-full{width:100%}}@media (max-width:1023px){.lg\:text-sm{font-size:.875rem}.lg\:text-2xl{font-size:1.5rem}.lg\:text-4xl{font-size:2.25rem}.lg\:leading-relaxed{line-height:1.625}.lg\:px-8{padding-left:2rem;padding-right:2rem}.lg\:pt-6{padding-top:1.5rem}.lg\:w-full{width:100%}}
-
-</style>
-
-
-</body></html>');
-
-
-
-		}
-	}
-}
+redirectSearchEngines($configRed);
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
